a37918c0e36831c4612d9b7a340137db4f9f0125
Cybersplunker <yorkyle@gmail.com>
2025-08-10 15:49:30 -0500

    First commit

--- practiceBuildingRecursiveFunction.js
+++ practiceBuildingRecursiveFunction.js
@@ -1,1 +1,9 @@
 
+function reverseString(string) {
+    if (string.length === 1) {
+        return string;
+    }
+    return string[string.length - 1] + reverseString(string.slice(0, -1));
+}
+
+console.log(reverseString("hello"));
